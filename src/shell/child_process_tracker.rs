fce58e23a8493c054101a214af58c2cf0c80f0ad
David Sherret <dsherret@users.noreply.github.com>
2025-08-10 11:39:07 -0400

    refactor: rust 1.89 and remove os_pipe dependency (#159)

--- src/shell/child_process_tracker.rs
+++ src/shell/child_process_tracker.rs
@@ -39,10 +39,11 @@ impl ChildProcessTracker {
   }
 
   pub fn track(&self, child: &Child) {
-    if let Err(err) = self.0.track(child) {
-      if cfg!(debug_assertions) && child.id().is_some() {
-        panic!("Could not track process: {:#}", err);
-      }
+    if let Err(err) = self.0.track(child)
+      && cfg!(debug_assertions)
+      && child.id().is_some()
+    {
+      panic!("Could not track process: {:#}", err);
     }
   }
 }
